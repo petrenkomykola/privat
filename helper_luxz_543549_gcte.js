832c622f2069b88133d74b1447236d9317f7a9fc
petrenkomykola <petrenkomykola0@gmail.com>
2024-04-13 13:57:30 +0300

    Changed user info variable

---
+++ helper_luxz_543549_gcte.js
@@ -0,0 +1,4 @@
+83 / false
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+
